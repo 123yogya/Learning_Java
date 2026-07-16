d0842dba708cf06e8483d8b9a1338c9dbb35a07b
123yogya <shindeyogesh72278@gmail.com>
2026-07-16 14:12:36 +0530

    Inner class
    Inner class is a class that define inside another class

--- GarbageCollector.java
+++
@@ -1,8 +0,0 @@
-public class GarbageCollector {
-
-    public static void main(String[] args) {
-        String str =  new String("Hello");
-        str = null; // Object is no longer referenced
-        System.gc(); // Request the jvm to run Garbage Collection
-    }
-}
